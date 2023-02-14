d763054ed6ec7dd78915be7f53b2c4fb477cd28e
Rose Le <rosehle@protonmail.com>
2023-02-13 20:28:50 -0800

    Modifying CORS

--- src/index.js
+++ src/index.js
@@ -10,13 +10,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH"
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 app.use("/api/medications", medicationRouter);
