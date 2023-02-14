01debbfe408829e230a2caa5d0b9eeaa7aefb4a0
Rose Le <rosehle@protonmail.com>
2023-02-13 20:22:59 -0800

    Setting up CORS w/o CORS library

--- src/index.js
+++ src/index.js
@@ -9,7 +9,16 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use("/api/medications", medicationRouter);
 app.use("/api/users", userRouter);
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const cors = require("cors");
+
 const {
   signUpUser,
   loginUser,
